2dcb7b09657fa8dfd149f33517a5ae4e21e1d644
Sadman Hasan <sadman.h@live.com>
2020-01-10 09:15:41 -0500

    done- home view data fetched

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from personal.models import Products
 
-
 # Create your views here.
 
+context = {}
+products = Products.objects.all()
+
+
 def home_view(request):
-    return render(request, "home.html", {})
+    context['products'] = products
+    return render(request, "home.html", context)
 
 
 def details_view(request):
-    context = {}
-    products = Products.objects.all()
     context['products'] = products
     return render(request, "details.html", context)
