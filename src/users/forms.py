830aba533e0bb5c09b377102e047bb1b9c64f488
Sadman Hasan <sadman.h@live.com>
2020-01-12 07:52:22 -0500

    done- login

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
-
 from users.models import Users
 
 
@@ -10,3 +10,18 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = Users
         fields = ("email", "username", "password1", "password2")
+
+
+class UserAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Users
+        fields = ('username', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            username = self.cleaned_data['username']
+            password = self.cleaned_data['password']
+            if not authenticate(username=username, password=password):
+                raise forms.ValidationError("Invalid login")
