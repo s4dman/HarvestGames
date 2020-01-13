465a3fb3973dd9ccf24fcaa1067af4d7e61071bb
Sadman Hasan <sadman.h@live.com>
2020-01-13 15:01:57 -0500

    fixed- menubar

--- src/personal/models.py
+++ src/personal/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse_lazy
 
 AVAILABILITY = [
     ("In Stock", "in-stock"),
@@ -14,6 +15,9 @@ class Products(models.Model):
     price = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     availability = models.CharField(max_length=20, choices=AVAILABILITY)
+
+    def get_absolute_url(self):
+        return reverse_lazy('details', kwargs={'product_id': self.id})
 
     def __str__(self):
         return self.title
