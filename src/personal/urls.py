1ce09de7f9624f853cd72230faee4c5794da4c80
Sadman <sadman.h@live.com>
2020-07-19 04:35:35 -0400

    added- product category view

--- src/personal/urls.py
+++ src/personal/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import home_view, details_view, products_view, products_category_view
+from .views import home_view, details_view, products_view
 
 app_name = 'personal'
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('products/', products_view, name='products'),
-    path('products/<category>', products_category_view, name='products'),
-    path('products/<str:slug>/', details_view, name='details')
+    path('<str:category>/', products_view, name='products'),
+    path('product/<str:slug>/', details_view, name='details')
 ]
--- src/personal/views.py
+++ src/personal/views.py
@@ -8,14 +8,10 @@ def home_view(request):
     return render(request, "home.html")
 
 
-def products_view(request):
-    products = Products.objects.all()
-    context['products'] = products
-    return render(request, "products.html", context)
-
-
-def products_category_view(request, category):
-    products = Products.objects.all(category=category)
+def products_view(request, category):
+    products = Products.objects.filter(category=category)
+    first_product = Products.objects.filter(category=category)[:1].get()
+    context['category'] = first_product.category
     context['products'] = products
     return render(request, "products.html", context)
 
