2bf08e49dd9311912b02df7dfac37a067372ac9c
Sadman <sadman.h@live.com>
2020-07-19 02:35:06 -0400

    added- new UI, slugify url, category model

--- src/personal/urls.py
+++ src/personal/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, details_view, products_view
+from .views import home_view, details_view, products_view, products_category_view
 
 app_name = 'personal'
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('products/', products_view, name='products'),
-    path('products/<slug>/', details_view, name='details')
+    path('products/<category>', products_category_view, name='products'),
+    path('products/<str:slug>/', details_view, name='details')
 ]
