d7b53c027e37bb90fd03ae433b3cd8139602d58d
Sadman <sadman.h@live.com>
2020-07-23 14:17:02 -0400

    done- login/register UI

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- src/personal/views.py
+++ src/personal/views.py
@@ -5,13 +5,15 @@ context = {}
 
 
 def home_view(request):
-    return render(request, "home.html")
+    context['nbar'] = 'home'
+    return render(request, "home.html", context)
 
 
 def all_products_view(request):
     try:
         products = Products.objects.all()
         context['products'] = products
+        context['nbar'] = 'products'
         return render(request, "products.html", context)
     except Products.DoesNotExist:
         return render(request, "404.html")
@@ -22,10 +24,12 @@ def products_view(request, category):
     first_product = Products.objects.filter(category=category)[:1].get()
     context['category'] = first_product.category
     context['products'] = products
+    context['nbar'] = 'products'
     return render(request, "products.html", context)
 
 
 def details_view(request, slug):
     product = get_object_or_404(Products, slug=slug)
     context['product'] = product
+    context['nbar'] = 'products'
     return render(request, "details.html", context)
